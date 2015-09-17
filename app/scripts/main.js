e6d5bef2d236e870c021d69b85c39907964ac0a7
Taz Carper <jtc006@gmail.com>
2015-09-17 16:50:40 -0500

    Started Product page cap animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,63 +1,76 @@
 // http://gregfranko.com/blog/i-love-my-iife/
 (function(library) {
-    // Calls the second IIFE and locally passes in the global jQuery, window, and document objects
-    library(window, document, window.jQuery);
-  }
+		// Calls the second IIFE and locally passes in the global jQuery, window, and document objects
+		library(window, document, window.jQuery);
+	}
 
-  (function(window, document, $) {
-  	var headerContainer = $('.headerContainer');
-  	// lazy loader INIT
-  	//var layzr = new Layzr();
+	(function(window, document, $) {
+		var headerContainer = $('.headerContainer');
+		// lazy loader INIT
+		//var layzr = new Layzr();
 
-    var isLateralNavAnimating = false;
+		var isLateralNavAnimating = false;
 
-    var toggleFAQ = function(faq){
-      $(faq).next('.answer').toggleClass('open');
-    };
+		var toggleFAQ = function(faq) {
+			$(faq).next('.answer').toggleClass('open');
+		};
 
-    // on ready
-    $(function() {
+		// on ready
+		$(function() {
 
-    	// header scroll
-    	window.addEventListener('scroll', function(e) {
-    		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-    			shrinkOn = 200;
+			// header scroll
+			window.addEventListener('scroll', function(e) {
+				var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+					shrinkOn = 200;
 
-    		if (distanceY > shrinkOn) {
-    			headerContainer.addClass('smaller');
-    		} else {
-    			if (headerContainer.hasClass('smaller')) {
-    				headerContainer.removeClass('smaller');
-    			}
-    		}
-    	});
+				if (distanceY > shrinkOn) {
+					headerContainer.addClass('smaller');
+				} else {
+					if (headerContainer.hasClass('smaller')) {
+						headerContainer.removeClass('smaller');
+					}
+				}
+			});
 
-    	//open/close lateral navigation
-    	$('.cd-nav-trigger').on('click', function(event) {
-    		event.preventDefault();
+			//open/close lateral navigation
+			$('.cd-nav-trigger').on('click', function(event) {
+				event.preventDefault();
 
-    		console.log('clicked')
-    		//stop if nav animation is running
-    		if( !isLateralNavAnimating ) {
-    			if($(this).parents('.csstransitions').length > 0 ) {
-    				isLateralNavAnimating = true;
-    			}
+				console.log('clicked')
+					//stop if nav animation is running
+				if (!isLateralNavAnimating) {
+					if ($(this).parents('.csstransitions').length > 0) {
+						isLateralNavAnimating = true;
+					}
 
-    			$('html').toggleClass('navigation-is-open');
-    			console.log($(window).height());
+					$('html').toggleClass('navigation-is-open');
+					console.log($(window).height());
 
-    			$('.headerMain').toggleClass('makeBlack');
-    			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-    				//animation is over
-    				isLateralNavAnimating = false;
-    			});
-    		}
-    	});
+					$('.headerMain').toggleClass('makeBlack');
+					$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+						//animation is over
+						isLateralNavAnimating = false;
+					});
+				}
+			});
 
-      $('.faqContainer .question').on('click', function(event){
-        toggleFAQ(event.target);
-      });
+			$('.faqContainer .question').on('click', function(event) {
+				toggleFAQ(event.target);
+			});
 
-    }); // end ready
-  })
+			// Product Page Animation
+			if ($('.productPage')[0]) {
+				var time = 0;
+				$('.cap').each(function(i, v) {
+					console.log(v);
+					setTimeout(function(){$(v).addClass('shown');},time);
+					time += 105;
+					console.log('go');
+				});
+			}
+
+
+
+		}); // end ready
+	})
 );
