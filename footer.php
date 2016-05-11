bc166e82061d855dc0cbb4b0d43592ecbbfd8434
Taz Carper <jtc006@gmail.com>
2016-05-11 14:22:03 -0500

    no message

--- footer.php
+++ footer.php
@@ -86,7 +86,7 @@
 <script src="http://maps.google.com/maps/api/js?libraries=places,geometry"></script>
 <!--<script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/plugins.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/app/scripts/main.js"></script>-->
-<!-- 
+
 <script src="<?php echo get_template_directory_uri(); ?>/app/scripts/map/jplist.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/app/scripts/map/jlocator/jlocator.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/app/scripts/map/jlocator/controller/controller.js"></script>
@@ -114,8 +114,9 @@ s/bootstrap/dropdown.js"></script>
 
 <script src="<?php echo get_template_directory_uri(); ?>/app/scripts/contact/nlform.js"></script>
 
-<script src="<?php echo get_template_directory_uri(); ?>/app/scripts/main.js"></script> -->
+<script src="<?php echo get_template_directory_uri(); ?>/app/scripts/main.js"></script> 
 
-<script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/plugins.js"></script> 
+<!-- 
 <script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/map.js"></script> 
-<script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/main.js"></script> 
+<script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/plugins.js"></script> 
+<script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/main.js"></script>  -->
