bb653e4539a03b1a5702da1d2c8bfe1a88e64413
Taz Carper <jtc006@gmail.com>
2016-08-18 17:04:13 -0500

    no message

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -309,8 +309,8 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app/layouts'));
 });
 
-// gulp.task('build', ['html', 'fonts', 'extras'], () => {
-gulp.task('build', ['html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['html', 'fonts', 'extras'], () => {
+// gulp.task('build', ['html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({
     title: 'build',
     gzip: true
